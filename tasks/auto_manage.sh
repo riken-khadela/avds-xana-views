41668284debec2ba3400bdab706c1805a4d64b73
riken-khadela <rikenkhadelamain@gmail.com>
2024-06-11 10:45:21 +0530

    added automanage sh

---
+++ tasks/auto_manage.sh
@@ -0,0 +1,22 @@
+# project path
+cd ~/workspace/surviral-insta/
+. env/bin/activate
+. tasks/environment.sh
+
+export CURRENT_DIR=`dirname $(readlink -f $0)`
+export PRJ_DIR=`dirname $CURRENT_DIR`
+# go to project root directory
+cd $PRJ_DIR
+
+cd ~/workspace/avds-xana-views/
+
+
+# Replace the line in environment.sh
+sed -i '/export LESSCLOSE=\/usr\/bin\/lesspipe %s %s/d' tasks/environment.sh
+
+
+killall -9 python qemu-system-x86_64
+
+
+python manage.py login &
+python manage.py chat
